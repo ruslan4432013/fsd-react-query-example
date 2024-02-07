957a06dd71096d35e987df1f426b485cf0bb18fd
ruslan4432013 <82005595+ruslan4432013@users.noreply.github.com>
2024-02-07 16:32:05 +0300

    refactor: move queryClient to shared

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { router } from "./router";
-import { QueryClient } from "@tanstack/react-query";
 import { Providers } from "@/app/providers";
+import { queryClient } from "@/shared/api/query-client";
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5 * 60 * 1000,
-      gcTime: 5 * 60 * 1000,
-    }
-  }
-})
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ src/shared/api/query-client.ts
@@ -0,0 +1,10 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 5 * 60 * 1000,
+    }
+  }
+})
